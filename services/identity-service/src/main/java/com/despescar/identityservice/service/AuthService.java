78357fe0bbab44a42d0de4eed359035afdfd9126
MelinaCora <melinaccora97@gmail.com>
2026-06-09 16:47:39 -0300

    authService

--- services/identity-service/src/main/java/com/despescar/identityservice/service/AuthService.java
+++ services/identity-service/src/main/java/com/despescar/identityservice/service/AuthService.java
@@ -1,5 +1,30 @@
 package com.despescar.identityservice.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.despescar.identityservice.dto.request.LoginRequest;
+import com.despescar.identityservice.dto.response.LoginResponse;
+import com.despescar.identityservice.entity.User;
+import com.despescar.identityservice.repository.UserRepository;
+
+@Service
 public class AuthService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthService(
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder) {
+
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public LoginResponse login(
+            LoginRequest request) {
+
+        return null;
+    }
 }
