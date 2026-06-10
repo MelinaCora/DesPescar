9aa52caadb3fae320fa634f0ace211762ec6ee7d
MelinaCora <melinaccora97@gmail.com>
2026-06-10 00:02:18 -0300

    init flightservice

---
+++ services/flightservice/src/test/java/com/despescar/flightservice/FlightserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.despescar.flightservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
