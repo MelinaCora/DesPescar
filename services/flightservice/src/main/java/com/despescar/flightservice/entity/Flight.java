01848128c8edb0589688998684db57bf0faf17f0
MelinaCora <melinaccora97@gmail.com>
2026-06-26 14:01:00 -0300

    Agregamos archivos a Flight-Service

---
+++ services/flightservice/src/main/java/com/despescar/flightservice/entity/Flight.java
@@ -0,0 +1,5 @@
+package com.despescar.flightservice.entity;
+
+public class Flight {
+
+}
---
+++ services/flightservice/src/main/java/com/despescar/flightservice/repository/FlightRepository.java
@@ -0,0 +1,10 @@
+package com.despescar.flightservice.repository;
+
+public class FlightRepository {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
