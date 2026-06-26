1166700fd176704b8396d5809007b921aecaf8c2
MelinaCora <melinaccora97@gmail.com>
2026-06-26 15:48:34 -0300

    Request y response payment

---
+++ services/payment-service/src/main/java/com/despescar/payment_service/dto/request/PaymentRequest.java
@@ -0,0 +1,6 @@
+package com.despescar.payment_service.dto.request;
+
+public class PaymentRequest {
+	//entrada de datos
+
+}
---
+++ services/payment-service/src/main/java/com/despescar/payment_service/dto/response/PaymentResponse.java
@@ -0,0 +1,6 @@
+package com.despescar.payment_service.dto.response;
+
+public class PaymentResponse {
+	//molde de respuesta
+
+}
