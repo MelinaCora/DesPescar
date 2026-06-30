3a130288ca9d2990110616f9664c2b78ec3b12ee
MelinaCora <melinaccora97@gmail.com>
2026-06-30 15:33:20 -0300

    Configuración inicial de Flight Service y Swagger

--- services/flightservice/src/main/java/com/despescar/flightservice/controller/FlightController.java
+++ services/flightservice/src/main/java/com/despescar/flightservice/controller/FlightController.java
@@ -1,5 +1,13 @@
 package com.despescar.flightservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FlightController {
-	//el controller recibe las peticiones HTTP
+
+    @GetMapping("/test")
+    public String test() {
+        return "Flight Service funcionando correctamente";
+    }
 }
