ff055ab9fb38f4b19d5d63716e4868b657008084
MelinaCora <melinaccora97@gmail.com>
2026-06-30 21:49:12 -0300

    configuracion inicial de swagger en payment-service

---
+++ services/payment-service/src/main/java/com/despescar/payment_service/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.despescar.payment_service.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("DesPescar - Package Service API")
+                                .version("1.0")
+                                .description("Microservicio encargado de la gestión de vuelos.")
+                );
+    }
+}
--- services/payment-service/src/main/java/com/despescar/payment_service/controller/PaymentController.java
+++ services/payment-service/src/main/java/com/despescar/payment_service/controller/PaymentController.java
@@ -1,5 +1,14 @@
 package com.despescar.payment_service.controller;
 
-public class PaymentController {
-	//recibe las peticiones HTTP
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+	class PaymentController {
+	
+	@GetMapping("/test")
+    public String test() {
+        return "Payment Service funcionando correctamente";
+
+	}
 }
