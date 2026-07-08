e846ccc851426f5376750f7ae19e7c3e9d7a8d84
MelinaCora <melinaccora97@gmail.com>
2026-07-08 18:43:21 -0300

    airlineRepository

--- services/flightservice/src/main/java/com/despescar/flightservice/repository/AirlineRepository.java
+++ services/flightservice/src/main/java/com/despescar/flightservice/repository/AirlineRepository.java
@@ -1,5 +1,16 @@
 package com.despescar.flightservice.repository;
 
-public class AirlineRepository {
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.despescar.flightservice.entity.Airline;
+
+public interface AirlineRepository extends JpaRepository<Airline, UUID> {
+
+    Optional<Airline> findByCode(String code);
+
+    Optional<Airline> findByName(String name);
 
 }
