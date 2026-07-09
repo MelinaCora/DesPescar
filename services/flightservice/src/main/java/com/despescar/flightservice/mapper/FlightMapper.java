ffac51191b9fc062bef9f370283d8a491400017f
carla <carlarominagaleano@gmail.com>
2026-07-09 13:08:59 -0300

    acreamos el mapper de flight

--- services/flightservice/src/main/java/com/despescar/flightservice/mapper/FlightMapper.java
+++ services/flightservice/src/main/java/com/despescar/flightservice/mapper/FlightMapper.java
@@ -1,5 +1,28 @@
 package com.despescar.flightservice.mapper;
 
+import org.springframework.sterotype.Component;
+import com.despescar.flightservce.dto..airlines.request.AirlineRequest;
+import com.despescar.fligthservice.dto.airlines.response.AirlineResponse;
+import com.despescar.flightservice.entity.Airline;
+
+@Component
 public class FlightMapper {
-	//para crear el mapper
+    public Airline toEntity(AirlineRequest request) {
+        return Airline.builder()
+                .name(request.getName())
+                .code(request.getCode())
+                .country(request.getCountry())
+                .logoUrl(request.getLogoUrl())
+                .build()
+    }
+    public AirlineResponse toResponse(Airline airline){
+        return AirlineResponse.builder()
+                .id(airline.getId())
+                .name(airline.getName())
+                .code(airline.getCode())
+                .country(airline.getCountry())
+                .logoUrl(airline.getLogoUrl())
+                .build()
+    }
+
 }
