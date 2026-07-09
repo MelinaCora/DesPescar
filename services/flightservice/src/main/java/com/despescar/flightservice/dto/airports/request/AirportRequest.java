61a8eaea00d6dea3c2bce3d9717434d3f6f519fb
Martin Perugorria <hector.-martin@hotmail.com>
2026-07-08 23:02:11 -0300

    Creacion de Airport Request

--- services/flightservice/src/main/java/com/despescar/flightservice/dto/airports/request/AirportRequest.java
+++ services/flightservice/src/main/java/com/despescar/flightservice/dto/airports/request/AirportRequest.java
@@ -1,5 +1,15 @@
-package com.despescar.flightservice.dto.request;
+package com.despescar.flightservice.dto.airports.request.;
+
+import lombok.Data;
 
 public class AirportRequest {
+
+    private String name;
+
+    private String code;
+
+    private String city;
+
+    private String country;
 
 }
