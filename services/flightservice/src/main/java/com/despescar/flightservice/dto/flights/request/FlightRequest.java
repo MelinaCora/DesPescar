54dc6bbfbbd12b2f0ae9551cb1799c628b169351
Martin Perugorria <hector.-martin@hotmail.com>
2026-07-08 22:11:41 -0300

    Creacion de FlighRequiest en DTO

--- services/flightservice/src/main/java/com/despescar/flightservice/dto/flights/request/FlightRequest.java
+++ services/flightservice/src/main/java/com/despescar/flightservice/dto/flights/request/FlightRequest.java
@@ -1,5 +1,30 @@
-package com.despescar.flightservice.dto.request;
+package com.despescar.flightservice.dto.flights.request;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import lombok.Data;
+
+@Data
 
 public class FlightRequest {
-	//entrada de datos 
+	//entrada de datos
+    private String flightNumber;
+
+    private UUID airlineId;
+
+    private UUID originAirportID;
+
+    private UUID destinationAirportID;
+
+    private LocalDateTime departureTime;
+
+    private LocalDateTime arrivalTime;
+
+    private BigDecimal price;
+
+    private Integer avaibleSeates;
+
+
 }
--- services/flightservice/src/main/java/com/despescar/flightservice/repository/AirportRepository.java
+++ services/flightservice/src/main/java/com/despescar/flightservice/repository/AirportRepository.java
@@ -2,6 +2,7 @@ package com.despescar.flightservice.repository;
 
 import java.util.Optional;
 import java.util.UUID;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
