93a67a9c36a761bb3bd1f84b0fa309fc64ca4a7b
MelinaCora <melinaccora97@gmail.com>
2026-07-08 22:24:01 -0300

    Agregamos la clase FlightResponse en el DTO

--- services/flightservice/src/main/java/com/despescar/flightservice/dto/flights/response/FlightResponse.java
+++ services/flightservice/src/main/java/com/despescar/flightservice/dto/flights/response/FlightResponse.java
@@ -1,7 +1,36 @@
-package com.despescar.flightservice.dto.response;
+package com.despescar.flightservice.dto.flights.response;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.despescar.flightservice.enums.FlightStatus;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class FlightResponse {
-	
-	//modelado de respuesta
+
+    private UUID id;
+
+    private String flightNumber;
+
+    private String airline;
+
+    private String originAirport;
+
+    private String destinationAirport;
+
+    private LocalDateTime departureTime;
+
+    private LocalDateTime arrivalTime;
+
+    private BigDecimal price;
+
+    private Integer availableSeats;
+
+    private FlightStatus status;
 
 }
