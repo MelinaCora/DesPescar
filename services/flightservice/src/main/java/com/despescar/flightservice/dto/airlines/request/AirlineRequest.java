c2f6cbd9d7c96e76dfa2f0ded0add494acaf64cf
carla <carlarominagaleano@gmail.com>
2026-07-08 22:42:59 -0300

    agregamos la clase airlaine request

--- services/flightservice/src/main/java/com/despescar/flightservice/dto/airlines/request/AirlineRequest.java
+++ services/flightservice/src/main/java/com/despescar/flightservice/dto/airlines/request/AirlineRequest.java
@@ -1,5 +1,17 @@
-package com.despescar.flightservice.dto.request;
+package com.despescar.flightservice.dto.airlines.request;
+
+import lombok.Data;
+
+@Data
+
 
 public class AirlineRequest {
+    private String name;
+
+    private String code;
+
+    private String country;
+
+    private String logoUrl;
 
 }
