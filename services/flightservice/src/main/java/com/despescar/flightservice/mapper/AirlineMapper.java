e9d75243da5aadcba6a9610e6a9a18430b3ea1e0
carla <carlarominagaleano@gmail.com>
2026-07-09 13:17:52 -0300

    añadi el mapper de airline correctamente

--- services/flightservice/src/main/java/com/despescar/flightservice/mapper/AirlineMapper.java
+++ services/flightservice/src/main/java/com/despescar/flightservice/mapper/AirlineMapper.java
@@ -1,5 +1,31 @@
 package com.despescar.flightservice.mapper;
 
+import org.springframework.sterotype.Component;
+import com.despescar.flightservce.dto..airlines.request.AirlineRequest;
+import com.despescar.fligthservice.dto.airlines.response.AirlineResponse;
+import com.despescar.flightservice.entity.Airline;
+
+@Component
 public class AirlineMapper {
+    public Airline toEntity(AirlineRequest request) {
+        return Airline.builder()
+                .name(request.getName())
+                .code(request.getCode())
+                .country(request.getCountry())
+                .logoUrl(request.getLogoUrl())
+                .build()
+    }
+    public AirlineResponse toResponse(Airline airline){
+        return AirlineResponse.builder()
+                .id(airline.getId())
+                .name(airline.getName())
+                .code(airline.getCode())
+                .country(airline.getCountry())
+                .logoUrl(airline.getLogoUrl())
+                .build()
+    }
 
 }
+
+
+
