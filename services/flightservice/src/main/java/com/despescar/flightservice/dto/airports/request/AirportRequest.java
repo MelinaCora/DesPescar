a667886f892e26163994544bd586bc5ea8b89d11
MelinaCora <melinaccora97@gmail.com>
2026-07-14 09:48:18 -0300

    Creacion de AirportController

--- services/flightservice/src/main/java/com/despescar/flightservice/dto/airports/request/AirportRequest.java
+++ services/flightservice/src/main/java/com/despescar/flightservice/dto/airports/request/AirportRequest.java
@@ -1,16 +1,23 @@
 package com.despescar.flightservice.dto.airports.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class AirportRequest {
 
+    @NotBlank
     private String name;
 
+    @NotBlank
+    @Size(min = 3, max = 3)
     private String code;
 
+    @NotBlank
     private String city;
 
+    @NotBlank
     private String country;
 
 }
